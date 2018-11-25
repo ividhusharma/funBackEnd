d210c192915bc8276595d9f24a459b9e82436001
Vidhu Shekhar <mevidhusharma@gmail.com>
2018-11-25 12:51:52 +0530

    Create PatientRepository.java

---
+++ src/main/java/com/aarpee/startfun/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package com.aarpee.startfun.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.aarpee.startfun.Entity.Patient;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface PatientRepository extends CrudRepository<Patient, Long> {
+
+}
