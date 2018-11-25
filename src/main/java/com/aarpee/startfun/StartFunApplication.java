018bf98da2f6626f1920cd6bacca902578f8c592
Vidhu Shekhar <mevidhusharma@gmail.com>
2018-11-25 12:53:58 +0530

    Add files via upload

---
+++ src/main/java/com/aarpee/startfun/StartFunApplication.java
@@ -0,0 +1,12 @@
+package com.aarpee.startfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartFunApplication.class, args);
+	}
+}
