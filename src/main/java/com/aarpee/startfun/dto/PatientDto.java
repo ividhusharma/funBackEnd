11d6ae2f6a59b0856fcbd08c3ebae921ebda9dfc
Vidhu Shekhar <mevidhusharma@gmail.com>
2018-11-25 12:49:17 +0530

    Create PatientDto.java

---
+++ src/main/java/com/aarpee/startfun/dto/PatientDto.java
@@ -0,0 +1,64 @@
+package com.aarpee.startfun.dto;
+
+public class PatientDto {
+
+	private Long userId;
+	
+	private String firstName;
+    
+    private String lastName;
+    
+    private Long mobileNumber;
+
+    private String address;
+    
+    private String email;
+    
+    public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Long getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(Long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+}
